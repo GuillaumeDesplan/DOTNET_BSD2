31dfb70292fd83dad3d5cca7d618c1c05cb06f6a
cevig <cevig@PC-CEVIVAL>
2025-02-07 15:32:37 +0100

    Ajoutez des fichiers projet.

---
+++ BSD2_24/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BSD2_24.Data;
+using BSD2_24.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BSD2_24Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BSD2_24Context") ?? throw new InvalidOperationException("Connection string 'BSD2_24Context' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initalize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
